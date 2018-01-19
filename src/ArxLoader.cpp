b8ba05a34ffcdfaae22b7a8a6f4f62795be408dc
Sam Kingdon <smarky55@gmail.com>
2018-01-19 19:59:29 +0000

    Fix lack of comments on license stub in source files

--- src/ArxLoader.cpp
+++ src/ArxLoader.cpp
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
-=======
-This Source Code Form is subject to the terms of the Mozilla Public
-License, v. 2.0. If a copy of the MPL was not distributed with this
-file, You can obtain one at http://mozilla.org/MPL/2.0/.
->>>>>>> 2bbb38892a1a5653b0034c5f1fd909c91fcbd22d
 
 #include "ArxLoader.h"
 
